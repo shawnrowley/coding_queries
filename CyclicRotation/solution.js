7a7b237a2135b24a71e2ca75a0f4c9907dcfcde1
shawnrowley <shawn.a.rowley@gmail.com>
2021-03-21 15:48:52 -0400

    Codiltiy CyclicRotation solution

---
+++ CyclicRotation/solution.js
@@ -0,0 +1,29 @@
+// you can write to stdout for debugging purposes, e.g.
+// console.log('this is a debug message');
+
+function getShift(shift, A) {
+  if (shift >= A.length) {
+    shift = shift - (A.length)
+  }
+  return (shift >= A.length) ? getShift(shift, A) : shift
+}
+
+function solution(A, K) {
+    // write your code in JavaScript (Node.js 8.9.4)
+
+    if (A.length === K) {
+        return A;
+    }
+
+    const shifted = A.slice(0);
+    let index = 0;
+    let shift = 0;
+    while (index < A.length) {
+      shift = index + K
+      shift = getShift(shift, A);
+      shifted[shift] = A[index]
+      index++
+    }
+
+    return shifted
+}
